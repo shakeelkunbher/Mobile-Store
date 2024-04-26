d24a0ae10e38a08fcbc489051774135218fd69d8
Shakeel Kunbher <151429638+shakeelkunbher@users.noreply.github.com>
2024-04-26 15:51:25 +0500

    Add files via upload

---
+++ Moblie Store/app.js
@@ -0,0 +1,431 @@
+var mobiles = {
+  iphone: {
+    iphone15pro: {
+      name: "Iphone 15 Pro",
+      ram: "8GB",
+      storage: "1TB",
+      camera: "48MP",
+      price: "432,000",
+      bettary: "3274mAH",
+      colours: ["#525661", "#54B4D3", "#E4A11B"],
+      image: "Pictures/Iphone/apple-iphone-15-pro-1.jpg",
+    },
+    iphone15plus: {
+      name: "Iphone 15 Plus",
+      ram: "6GB",
+      storage: "512GB",
+      camera: "48MP",
+      price: "332,000",
+      bettary: "4383mAH",
+      colours: ["green", "blue", "pink"],
+      image: "Pictures/Iphone/apple-iphone-15-plus-1.jpg",
+    },
+    iphone14pro: {
+      name: "Iphone 14 Pro",
+      ram: "6GB",
+      storage: "1TB",
+      camera: "48MP",
+      price: "332,000",
+      bettary: "3200mAH",
+      colours: ["#f1f3f2", "#f4e7cb", "#464342"],
+      image: "Pictures/Iphone/iphone 14 pro.jpg",
+    },
+    iphone14plus: {
+      name: "Iphone 14 Plus",
+      ram: "6GB",
+      storage: "512GB",
+      camera: "48MP",
+      price: "642,000",
+      battery: "4100mAh",
+      colours: ["#f1f3f2", "#f4e7cb", "#464342"],
+      image: "Pictures/Iphone/apple-iphone-14-plus-3.jpg",
+    },
+    iphone13pro: {
+      name: "Iphone 13 Pro",
+      ram: "6GB",
+      storage: "512GB",
+      camera: "12MP",
+      price: "412,000",
+      battery: "3095mAh",
+      colours: ["#fde9d0", "#4e4d49", "#96b0c9"],
+      image: "Pictures/Iphone/apple-iphone-13-pro-01.jpg",
+    },
+    iphone13plus: {
+      name: "Iphone 13 Plus",
+      ram: "6GB",
+      storage: "512GB",
+      camera: "12MP",
+      price: "132,000",
+      battery: "4352mAh",
+      colours: ["#96b0c9", "#4e4d49", "#fde9d0"],
+      image: "Pictures/Iphone/apple-iphone-13-plus.jpg",
+    },
+  },
+  samsung: {
+    galaxyA24ultra: {
+      name: "Samsung Galaxy A24 Ultra",
+      ram: "8GB",
+      storage: "256GB",
+      camera: "108MP",
+      price: "382,000",
+      battery: "5000mAh",
+      colours: ["#797982", "#eee600", "#eee0b1"],
+      image: "Pictures/Samsung/samsung-galaxy-s24-ultra-5g-sm-s928-0.jpg",
+    },
+    galaxyA25: {
+      name: "Samsung Galaxy A25",
+      ram: "6GB",
+      storage: "128GB",
+      camera: "48MP",
+      price: "492,000",
+      battery: "5000mAh",
+      colours: ["yellow", "#add8e6", "#31494f"],
+      image: "Pictures/Samsung/samsung-galaxy-a25-1.jpg",
+    },
+    galaxyA15: {
+      name: "Samsung Galaxy A15",
+      ram: "4GB",
+      storage: "64GB",
+      camera: "48MP",
+      price: "322,000",
+      battery: "5000mAh",
+      colours: ["#121121", "#add8e6", "color: rgb(65, 65, 249)"],
+      image: "Pictures/Samsung/samsung-galaxy-a15-lte-1.jpg",
+    },
+    galaxyA05s: {
+      name: "Samsung Galaxy A05s",
+      ram: "3GB",
+      storage: "32GB",
+      camera: "13MP",
+      price: "452,000",
+      battery: "5000mAh",
+      colours: ["#3eb489", "#41424c", "#6c5c80"],
+      image: "Pictures/Samsung/samsung-galaxy-a05s-1.jpg",
+    },
+    galaxyS23fe: {
+      name: "Samsung Galaxy S23 FE",
+      ram: "8GB",
+      storage: "128GB",
+      camera: "64MP",
+      price: "482,000",
+      battery: "4500mAh",
+      colours: ["#6c5c80", "#41424c", "#3eb489"],
+      image: "Pictures/Samsung/samsung-galaxy-s23-fe-1.jpg",
+    },
+    galaxyA54: {
+      name: "Samsung Galaxy A54",
+      ram: "6GB",
+      storage: "128GB",
+      camera: "64MP",
+      price: "234,666",
+      battery: "5000mAh",
+      colours: ["#b0bce5", "#e3b0b5", "#b3d49a"],
+      image: "Pictures/Samsung/samsung-galaxy-a54-5.jpg",
+    },
+  },
+  oppo: {
+    A78: {
+      name: "Oppo A78",
+      ram: "6GB",
+      storage: "128GB",
+      camera: "48MP",
+      price: "132,000",
+      battery: "4500mAh",
+      colours: ["rgb(65, 65, 249)", "#121121", "#add8e6"],
+      image: "Pictures/Oppo/oppo-a78.jpg",
+    },
+
+    oppoReno115G: {
+      name: " OPPO Reno11 5G",
+      ram: "4GB",
+      storage: "64GB",
+      camera: "16MP",
+      price: "32,000",
+      battery: "4000mAh",
+      colours: ["yellow", "#add8e6", "#31494f"],
+      image: "Pictures/Oppo/oppo-reno11-international-1.jpg",
+    },
+    A38: {
+      name: "Oppo A38",
+      ram: "3GB",
+      storage: "32GB",
+      camera: "13MP",
+      price: "42,000",
+      battery: "3500mAh",
+      colours: ["#b0bce5", "#e3b0b5", "#b3d49a"],
+      image: "Pictures/Oppo/oppo-a38.jpg",
+    },
+    A2Pro: {
+      name: "Oppo A2 Pro",
+      ram: "8GB",
+      storage: "256GB",
+      camera: "64MP",
+      price: "42,000",
+      battery: "4300mAh",
+      colours: ["yellow", "#add8e6", "#31494f"],
+      image: "Pictures/Oppo/oppo-a2-pro.jpg",
+    },
+    K11x: {
+      name: "Oppo K11x",
+      ram: "6GB",
+      storage: "128GB",
+      camera: "48MP",
+      price: "92,000",
+      battery: "4000mAh",
+      colours: ["rgb(65, 65, 249)", "#121121", "#add8e6"],
+      image: "Pictures/Oppo/k11x-01.jpg",
+    },
+    A58: {
+      name: "Oppo A58 5G",
+      ram: "3GB",
+      storage: "32GB",
+      camera: "13MP",
+      price: "22,000",
+      battery: "2900mAh",
+      colours: ["#b0bce5", "#b3d49a", "#e3b0b5"],
+      image: "Pictures/Oppo/oppo-a58.jpg",
+    },
+  },
+  infinix: {
+    note40: {
+      name: "Infinix Note 40",
+      ram: "8GB",
+      storage: "256GB",
+      camera: "64MP",
+      price: "55,000",
+      battery: "6000mAh",
+      colours: ["#b3d49a", "#e3b0b5", "#b0bce5"],
+      image: "Pictures/Infinix/note 40.png",
+    },
+    smart8plus: {
+      name: "Infinix Smart 8 Plus",
+      ram: "4GB",
+      storage: "64GB",
+      camera: "16MP",
+      price: "42,000",
+      battery: "5000mAh",
+      colours: ["rgb(65, 65, 249)", "#121121", "#add8e6"],
+      image: "Pictures/Infinix/smart8plus.png",
+    },
+    hot40i: {
+      name: "Infinix Hot 40i",
+      ram: "3GB",
+      storage: "32GB",
+      camera: "13MP",
+      price: "22,000",
+      battery: "4500mAh",
+      colours: ["yellow", "#add8e6", "#31494f"],
+      image: "Pictures/Infinix/hot40i.png",
+    },
+    zero30: {
+      name: "Infinix Zero 30",
+      ram: "8GB",
+      storage: "128GB",
+      camera: "48MP",
+      price: "44,000",
+      battery: "4500mAh",
+      colours: ["#b0bce5", "#e3b0b5", "#b3d49a"],
+      image: "Pictures/Infinix/zero30.png",
+    },
+    hot12play: {
+      name: "Infinix Hot 12 Play",
+      ram: "4GB",
+      storage: "64GB",
+      camera: "16MP",
+      price: "66,000",
+      battery: "6000mAh",
+      colours: ["rgb(65, 65, 249)", "#121121", "#add8e6"],
+      image: "Pictures/Infinix/hot12play.png",
+    },
+    zero20: {
+      name: "Infinix Zero 20",
+      ram: "8GB",
+      storage: "128GB",
+      camera: "48MP",
+      price: "62,000",
+      battery: "4500mAh",
+      colours: ["yellow", "#add8e6", "#31494f"],
+      image: "Pictures/Infinix/zero20.png",
+    },
+  },
+  vivo: {
+    Y27s: {
+      name: "Vivo Y27s",
+      ram: "8GB",
+      storage: "128GB",
+      camera: "64MP",
+      price: "52,000",
+      battery: "4500mAh",
+      colours: ["#add8e6", "#121121", "rgb(65, 65, 249)"],
+      image: "Pictures/Vivo/y27s.png",
+    },
+    v29e: {
+      name: "Vivo V29e 5G",
+      ram: "6GB",
+      storage: "256GB",
+      camera: "48MP",
+      price: "75,000",
+      battery: "4300mAh",
+      colours: ["black", "silver", "purple"],
+      image: "Pictures/Vivo/v29e5G.png",
+    },
+    Y03: {
+      name: "Vivo Y03",
+      ram: "4GB",
+      storage: "64GB",
+      camera: "13MP",
+      price: "85,000",
+      battery: "5000mAh",
+      colours: ["black", "blue", "green"],
+      image: "Pictures/Vivo/y03.png",
+    },
+    Y17s: {
+      name: "Vivo Y17s",
+      ram: "8GB",
+      storage: "128GB",
+      camera: "48MP",
+      price: "72,040",
+      battery: "4500mAh",
+      colours: ["black", "silver", "blue"],
+      image: "Pictures/Vivo/y17s.png",
+    },
+    Y02t: {
+      name: "Vivo Y02t",
+      ram: "4GB",
+      storage: "128GB",
+      camera: "13MP",
+      price: "70,000",
+      battery: "5000mAh",
+      colours: ["black", "green", "orange"],
+      image: "Pictures/Vivo/Y02t.png",
+    },
+    Y36: {
+      name: "Vivo Y36",
+      ram: "12GB",
+      storage: "256GB",
+      camera: "108MP",
+      price: "89,000",
+      battery: "5000mAh",
+      colours: ["black", "silver", "blue"],
+      image: "Pictures/Vivo/Y36.png",
+    },
+  },
+};
+var main = document.getElementById("main");
+var company = document.getElementById("company");
+var model = document.getElementById("model");
+
+function reanderAllCards() {
+  for (var key1 in mobiles) {
+    company.innerHTML += `<option value='${key1}'>${key1}</option>`;
+    for (var key2 in mobiles[key1]) {
+      var obj = mobiles[key1][key2];
+      // console.log(obj);
+      main.innerHTML += `
+      <div class="col-12 col-sm-1f2 col-md-6 col-lg-4 card-main">
+            <div class="card og-card d-flex align-items-center justify-content-center">
+              <img
+                src="${obj.image}"
+                class="card-img-top img-fluid I-img"
+                alt="..."
+              />
+              <div class="card-body text-center">
+                <div class="card-title d-inline">
+                  <i class="bi bi-circle-fill" style="color: ${obj.colours[0]}"></i>
+                  <i class="bi bi-circle-fill" style="color: ${obj.colours[1]}"></i>
+                  <i class="bi bi-circle-fill " style="color: ${obj.colours[2]}"></i>
+                 </div>
+                <div class = 'mt-5'>
+                  <p class="card-text1">${obj.name}</p>
+                  <p class="card-text2">${obj.price}</p>
+                </div>
+                </div>
+              <button class="btn button" style="background-color: #ff7043">
+                ADD TO CART
+              </button>
+            </div>
+          </div> `;
+    }
+  }
+}
+reanderAllCards();
+
+function setModelName() {
+  model.innerHTML = "";
+  model.innerHTML = `<option selected>Select Company</option>`;
+  var getName = mobiles[company.value];
+  for (var key in getName) {
+    // console.log(getName[key].name);
+    model.innerHTML += `<option value='${key}'>${getName[key].name}</option>`;
+  }
+}
+
+var resultCard = document.getElementById("result-card");
+
+function searching() {
+  // main.innerHTML = ''
+  main.style.display = "none";
+  resultCard.style.display = "flex";
+  resultCard.style.position = "initial";
+  // console.log(company.value, "====>", model.value);
+  var mobile = mobiles[company.value][model.value];
+
+  resultCard.innerHTML = `
+  <div
+        class="container-fluid d-flex justify-content-center align-items-center"
+      >
+        <div
+          class="card p-4 d-flex justify-content-center align-items-center"
+          style="width: 50%"
+        >
+          <div class="row g-0">
+            <div class="col-md-4 d-flex justify-content-center">
+              <img
+                src="${mobile.image}"
+                class="img-fluid rounded-start"
+                alt="..."
+                width="250px"
+                height="250px"
+                style="object-fit: contain"
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h3 class="card-title">${mobile.name}</h3>
+                <p>
+                  ${mobile.price}
+                  <br />
+                  ${mobile.ram + " Ram"}
+                  <br />
+                  ${mobile.storage + " Storage"}
+                </p>
+                <div class="card-title d-inline">
+                  <i
+                    class="bi bi-circle-fill"
+                    style="color: ${mobile.colours[0]}"
+                  ></i>
+                  <i
+                    class="bi bi-circle-fill"
+                    style="color: ${mobile.colours[1]}"
+                  ></i>
+                  <i
+                    class="bi bi-circle-fill"
+                    style="color: ${mobile.colours[2]}"
+                  ></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+          `;
+}
+
+function clearSearch() {
+  resultCard.innerHTML = "";
+  resultCard.style.display = "none";
+  main.style.display = "flex";
+  resultCard.style.position = "absolute";
+
+  // company.innerHTML = "<option selected>Select Company</option>";
+}
